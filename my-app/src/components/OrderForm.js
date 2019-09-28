03e07a2f9919f3ca1068d96a441850b59c8f85a7
apierc2175 <aidanwpierce@gmail.com>
2019-09-28 13:20:32 -0400

    styled the order form

--- my-app/src/components/OrderForm.js
+++ my-app/src/components/OrderForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MenuList from './MenuList';
-import { Row, Col, Grid, Container } from 'react-bootstrap';
+import { Row, Col, Grid, Container, Table } from 'react-bootstrap';
 
 class OrderForm extends Component {
 
@@ -18,17 +18,31 @@ class OrderForm extends Component {
   render() {
 
     let orderItems = this.props.orderItems.map((item, index) => (
-      <div key={index}>
-        <p>{item.name}</p>
-        <h1>{item.description}</h1>
-        <h1>{item.price}</h1>
-      </div>
+    //<div key={index}>
+      <tr>
+        <td>
+          <ul>
+            <li className="menu-item-name">{item.name}</li>
+            <li>{item.description}</li>
+          </ul>
+        </td>
+        <td>${item.price}</td>
+      </tr>
+
+
+
+
+      //</div>
     ));
 
     return (
       <section className="order-form">
         <h2>Order Form</h2>
-        {orderItems}
+        <Table striped bordered hover wide-width>
+          <tbody>
+            {orderItems}
+          </tbody>
+        </Table>
         <p>{this.props.subtotal}</p>
       </section>
     )
